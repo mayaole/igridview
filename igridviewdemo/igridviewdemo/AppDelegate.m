d4f4f20e2c7519cb5b3826c90efe93c94d7b9eba
Fung <fung@itcast.cn>
2015-03-22 12:55:44 +0800

    test

--- igridviewdemo/igridviewdemo/AppDelegate.m
+++ igridviewdemo/igridviewdemo/AppDelegate.m
@@ -13,6 +13,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+//
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     ViewController *viewContr = [[ViewController alloc] init];
